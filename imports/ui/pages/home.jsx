9b769a2bd1631c9f080f32749738b03e5dbd038f
tyherox <silverbae@gmail.com>
2017-01-29 00:24:00 +0900

    Update home.jsx

--- imports/ui/pages/home.jsx
+++ imports/ui/pages/home.jsx
@@ -128,7 +128,7 @@ export default class App extends Component {
                         <Buffer size ="30"/>
                     </Grid>
                     <div className = 'home_subscribe' ref = 'subscribe'>
-                        <Buffer size ="50"/>
+                        <Buffer size ="30"/>
                         <h1><b>COMING SOON!</b></h1>
                         <Buffer mega />
                         <form action="https://docs.google.com/a/ryestory.com/forms/d/e/1FAIpQLSc5UsKau_hGxbkUrH2PxXTBoaBh9BGyIWzHtBCQxK3hroO08w/formResponse"

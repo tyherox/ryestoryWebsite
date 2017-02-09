9e4d82e9c032c81e2426d1987063a8aa87323c12
43belita <43ryuflower@gmail.com>
2017-02-09 14:59:39 +0900

    save

--- imports/App.jsx
+++ imports/App.jsx
@@ -13,6 +13,7 @@ import './ui/stylesheets/text.css'
 import './ui/stylesheets/image.css'
 import './ui/stylesheets/blog.css'
 import './ui/stylesheets/about.css'
+import './ui/stylesheets/contactUs.css'
 import {Navbar, NavItem, MenuItem, Nav, NavDropdown} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
 import {browserHistory} from 'react-router';

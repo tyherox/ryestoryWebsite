5db7717e8c68b1f54faff8c020fdc945eb0a69d7
tyherox <silverbae@gmail.com>
2017-02-10 13:25:37 +0900

    Fixed Contact Functionality

--- imports/api/messages.js
+++ imports/api/messages.js
@@ -5,3 +5,16 @@
 import { Mongo } from 'meteor/mongo';
 
 export const Messages = new Mongo.Collection('messages');
+
+Meteor.methods({
+    'messages.insert'(name, email, message) {
+
+        console.log("Inserting:", name, email, message)
+
+        Messages.insert({
+            name: name,
+            email: email,
+            message: message
+        });
+    },
+});
--- imports/ui/pages/contactUs.jsx
+++ imports/ui/pages/contactUs.jsx
@@ -43,7 +43,7 @@ export default class App extends Component {
             name: name,
             email: email,
             message: message
-        })
+        });
 
         setTimeout(() => {
             this.setState({isLoading: false});
